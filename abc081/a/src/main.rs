174b39e74fa85bd6d4699b1a0b9e5a13ebd631a1
kgtkr <kgtkr.jp@gmail.com>
2017-12-23 18:08:16 +0900

    a

---
+++ abc081/a/src/main.rs
@@ -0,0 +1,23 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = run(input);
+    println!("{}", output);
+}
+
+fn run(input: String) -> String {
+    input.chars().filter(|&x| x == '1').count().to_string()
+}
+
+#[test]
+fn test() {
+    let tests = vec![("101", "2"), ("000", "0")];
+    for (i, &(input, output)) in tests.iter().enumerate() {
+        println!("test:{}", i);
+        assert_eq!(run(input.to_string()), output.to_string());
+    }
+}
