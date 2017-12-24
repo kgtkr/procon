18dea4e84b6ca377a2ceb8a566978edf55769763
kgtkr <kgtkr.jp@gmail.com>
2017-12-24 22:08:14 +0900

    標準偏差でやってみたけど無理、何故か出来ん

--- xmascon17/a/src/main.rs
+++ xmascon17/a/src/main.rs
@@ -1,3 +1,24 @@
+extern crate hound;
+
 fn main() {
-    println!("{}", "BABAAABAABAABAB");
+    for x in 1..16 {
+        let a = bun(&format!("input/{:>02}_A.wav", x));
+        let b = bun(&format!("input/{:>02}_B.wav", x));
+        print!("{}", if a > b { "B" } else { "A" });
+    }
+}
+
+
+//オーディオの分散
+fn bun(name: &str) -> i64 {
+    let mut reader = hound::WavReader::open(name).unwrap();
+    let vec = reader
+        .samples::<i16>()
+        .map(|x| x.unwrap())
+        .collect::<Vec<_>>();
+
+    let sum = vec.iter().fold(0i64, |sum, &i| sum + i as i64);
+    let count = vec.len() as i64;
+    let ave = sum / count;
+    vec.iter().map(|&x| (x as i64 - ave).pow(2)).sum::<i64>() / count
 }
