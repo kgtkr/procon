d426d86620144f656e1f223292550496cc30a414
kgtkr <kgtkr.jp@gmail.com>
2017-12-24 22:57:46 +0900

    Revert "色々やってみた"
    This reverts commit 523e0110592c98309af4faf564e7d300c272a460.

--- xmascon17/a/src/main.rs
+++ xmascon17/a/src/main.rs
@@ -1,8 +1,6 @@
 extern crate hound;
 
 fn main() {
-    println!("あり {}", bun("sample_mono_comp.wav"));
-    println!("なし {}", bun("sample_mono_dry.wav"));
     for x in 1..16 {
         let a = bun(&format!("input/{:>02}_A.wav", x));
         let b = bun(&format!("input/{:>02}_B.wav", x));
@@ -11,26 +9,16 @@ fn main() {
 }
 
 
-//オーディオの変化
-fn bun(name: &str) -> f64 {
+//オーディオの分散
+fn bun(name: &str) -> i64 {
     let mut reader = hound::WavReader::open(name).unwrap();
     let vec = reader
-        .samples::<i32>()
+        .samples::<i16>()
         .map(|x| x.unwrap())
         .collect::<Vec<_>>();
 
-    vec.iter()
-        .enumerate()
-        .map(|(i, &x)| match i {
-            0 => 0i64,
-            _ => ((x as i64) - (vec[i - 1] as i64)).abs(),
-        })
-        .fold(0i64, |sum, i| sum + i as i64) as f64 / ave(&vec)
-}
-
-fn ave(vec: &Vec<i32>) -> f64 {
-    let sum = &vec.iter()
-        .map(|x| x.abs())
-        .fold(0f64, |sum, i| sum + i as f64);
-    sum / vec.len() as f64
+    let sum = vec.iter().fold(0i64, |sum, &i| sum + i as i64);
+    let count = vec.len() as i64;
+    let ave = sum / count;
+    vec.iter().map(|&x| (x as i64 - ave).pow(2)).sum::<i64>() / count
 }
