3de04ff9c81e9871f3b12040fe1d0b69e8151d35
kgtkr <kgtkr.jp@gmail.com>
2017-12-25 14:45:20 +0900

    改善

--- xmascon17/a/src/main.rs
+++ xmascon17/a/src/main.rs
@@ -1,6 +1,9 @@
 extern crate hound;
 
 fn main() {
+    println!("あり {}", bun("sample_mono_comp.wav"));
+    println!("なし {}", bun("sample_mono_dry.wav"));
+
     for x in 1..16 {
         let a = bun(&format!("input/{:>02}_A.wav", x));
         let b = bun(&format!("input/{:>02}_B.wav", x));
@@ -14,7 +17,7 @@ fn bun(name: &str) -> i64 {
     let mut reader = hound::WavReader::open(name).unwrap();
     let vec = reader
         .samples::<i16>()
-        .map(|x| x.unwrap())
+        .map(|x| x.unwrap().abs())
         .collect::<Vec<_>>();
 
     let sum = vec.iter().fold(0i64, |sum, &i| sum + i as i64);
