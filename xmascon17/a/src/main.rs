7792ec8c40bd1fb091883af38c5933a8aab18f30
kgtkr <kgtkr.jp@gmail.com>
2017-12-25 14:53:24 +0900

    サンプルの絶対値の合計 駄目

--- xmascon17/a/src/main.rs
+++ xmascon17/a/src/main.rs
@@ -1,27 +1,24 @@
 extern crate hound;
 
 fn main() {
-    println!("あり {}", bun("sample_mono_comp.wav"));
-    println!("なし {}", bun("sample_mono_dry.wav"));
+    println!("あり {}", onryo("sample_mono_comp.wav"));
+    println!("なし {}", onryo("sample_mono_dry.wav"));
 
     for x in 1..16 {
-        let a = bun(&format!("input/{:>02}_A.wav", x));
-        let b = bun(&format!("input/{:>02}_B.wav", x));
-        print!("{}", if a > b { "B" } else { "A" });
+        let a = onryo(&format!("input/{:>02}_A.wav", x));
+        let b = onryo(&format!("input/{:>02}_B.wav", x));
+        print!("{}", if a > b { "A" } else { "B" });
     }
 }
 
 
-//オーディオの分散
-fn bun(name: &str) -> i64 {
+//音量の絶対値の合計
+fn onryo(name: &str) -> i64 {
     let mut reader = hound::WavReader::open(name).unwrap();
     let vec = reader
         .samples::<i16>()
-        .map(|x| x.unwrap().abs())
+        .map(|x| (x.unwrap() as i32).abs())
         .collect::<Vec<_>>();
 
-    let sum = vec.iter().fold(0i64, |sum, &i| sum + i as i64);
-    let count = vec.len() as i64;
-    let ave = sum / count;
-    vec.iter().map(|&x| (x as i64 - ave).pow(2)).sum::<i64>() / count
+    vec.iter().fold(0i64, |sum, &i| sum + i as i64)
 }
