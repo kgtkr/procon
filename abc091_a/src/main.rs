40017cfb7ccaf62d679bc5114b121d453d3b0ada
kgtkr <kgtkr.jp@gmail.com>
2018-03-17 21:02:32 +0900

    a

---
+++ abc091_a/src/main.rs
@@ -0,0 +1,42 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = run(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn run(input: String) -> String {
+    let list = input
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    if list[0] + list[1] >= list[2] {
+        "Yes"
+    } else {
+        "No"
+    }.to_string()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!(super::run($input.to_string()), $output.to_string());
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "50 100 120" => "Yes",
+    test2: "500 100 1000" => "No",
+    test3: "19 123 143" => "No",
+    test4: "19 123 142" => "Yes",
+}
