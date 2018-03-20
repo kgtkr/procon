0d51b81d7e3ebdcaafecee6862ee28755e2901ff
kgtkr <kgtkr.jp@gmail.com>
2018-03-20 18:21:22 +0900

    haskellテンプレ完成

--- _template-hs/Main.hs
+++ _template-hs/Main.hs
@@ -1,83 +1,66 @@
+import System.Environment
+import System.IO
+import Data.Maybe
+import Data.List
+import Control.Exception
+
 main :: IO ()
-main = undefined
+main = do
+  isTest <- getEnv "TEST" `catch` (\(SomeException _) -> return "")
+  if isTest == "1"
+    then do
+      testFile <- readFile "tests"
+      putStrLn $ tests testFile
+    else do
+      input <- getContents
+      putStrLn $ trim (solve (trim input))
 
 --処理
 solve :: String -> String
-solve = undefined
+solve s = show $ a + b where [a, b] = fmap read (words s)
 
-testParse :: String -> Maybe [(String, String)]
+tests :: String -> String
+tests text = case testParse text of
+  Right bodys -> unlines (fmap (\(i, body) -> test i body) (zip [1 ..] bodys))
+  Left  msg   -> "Test file parse error:" ++ msg
+
+test :: Int -> (String, String) -> String
+test n (input, outputRight)
+  | output == outputRight
+  = header ++ "Passed"
+  | otherwise
+  = header
+    ++ "Faild\n"
+    ++ "Right:"
+    ++ show outputRight
+    ++ "\nOutput:"
+    ++ show output
+ where
+  header = "test" ++ show n ++ ":"
+  output = trim $ solve input
+
+testParse :: String -> Either String [(String, String)]
 testParse s = case lines s of
   (sp1:sp2:bodys) -> testBodysParse sp1 sp2 bodys
-  _               -> Nothing
+  _               -> Left "testParse"
 
-testBodysParse :: String -> String -> [String] -> Maybe [(String, String)]
-testBodysParse sp1 sp2 bodys =
-  (sequence . fmap (testBodyParse sp2) . split sp1) bodys
+testBodysParse
+  :: String -> String -> [String] -> Either String [(String, String)]
+testBodysParse sp1 sp2 = (traverse . testBodyParse) sp2 . (split sp1)
 
-testBodyParse :: String -> [String] -> Maybe (String, String)
-testBodyParse sp body = case split sp body of
-  [input, output] -> Just (unlines input, unlines output)
-  _               -> Nothing
+testBodyParse :: String -> [String] -> Either String (String, String)
+testBodyParse sp body = case fmap (trim . unlines) (split sp body) of
+  [input, output] -> Right (input, output)
+  _               -> Left "testBodyParse"
 
 split :: Eq a => a -> [a] -> [[a]]
-split spratar body = foldr
-  (\x y -> if x == spratar then [] : y else let (p:ps) = y in (x : p) : ps)
-  [[]]
-  body
+split spratar = foldr f [[]]
+ where
+  f x (p:ps) | x == spratar = [] : p : ps
+             | otherwise    = (x : p) : ps
 
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
