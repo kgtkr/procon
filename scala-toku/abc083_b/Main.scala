2992343dc8f426c0b6849653d5a316dcb7f68176
kgtkr <kgtkr.jp@gmail.com>
2018-03-21 19:03:20 +0900

    ミス

--- scala-toku/abc083_b/Main.scala
+++ scala-toku/abc083_b/Main.scala
@@ -7,15 +7,18 @@ object Main{
       println(solve(input).trim());
     }
   }
-
+ 
   def solve(input:String):String={
     val List(n,a,b)=input.split(" ").map(_.toInt).toList;
+    (1 to n).filter(x=>bitween(x.toString().split("").filter(_.length!=0).map(_.toInt).sum,a,b)).sum.toString()
   }
-
+ 
+  def bitween(x:Int,min:Int,max:Int)=min<=x&&x<=max;
+ 
   val tests=List("""20 2 5""" -> """84""",
     """10 1 2""" -> """13""",
 """100 4 16"""->"""4554""");
-
+ 
   def test():String= {
     return tests.map{case (i,o)=>(i.trim(),o.trim())}.zipWithIndex.map{case ((input,outputExpect),i)=>{
       val output=solve(input).trim();
