e43a6d3673f2e7dbe7fde6e9e7b7f55706aac1af
kgtkr <kgtkr.jp@gmail.com>
2018-03-21 16:13:08 +0900

    テンプレ

---
+++ scala-toku/_template/Main.scala
@@ -0,0 +1,31 @@
+object Main{
+  def main(args: Array[String]){
+    if (sys.env.getOrElse("TEST", "")=="1"){
+      println(test());
+    }else{
+      val input=io.Source.stdin.getLines().mkString("\n");
+      println(solve(input).trim());
+    }
+  }
+
+  def solve(input:String):String={
+    input.split(" ").map(_.toInt).sum.toString()
+  }
+
+  val tests=List("""3 9""" -> """12""",
+    """31 32""" -> """63""",
+    """1 2""" -> """3""",
+    """-1 2""" -> """1""",
+    """10 1""" -> """11""");
+
+  def test():String= {
+    return tests.map{case (i,o)=>(i.trim(),o.trim())}.zipWithIndex.map{case ((input,outputExpect),i)=>{
+      val output=solve(input).trim();
+      s"test${i+1}:"+(if(output==outputExpect){
+        "Passed"
+      }else{
+        s"Failed\nexpect:\n${outputExpect}\noutput:\n${output}"
+      })
+    }}.mkString("\n");
+  }
+}
