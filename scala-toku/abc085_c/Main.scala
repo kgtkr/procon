06bf0d391b5b35d6b8b5f011eb4c468344dc3ee4
kgtkr <kgtkr.jp@gmail.com>
2018-03-21 22:16:51 +0900

    abc085_c

--- scala-toku/abc085_c/Main.scala
+++ scala-toku/abc085_c/Main.scala
@@ -8,39 +8,35 @@ object Main{
     }
   }
  
-  /*def solve(input:String):String={
-    val List(n,y)=input.split(" ").map(_.toInt).toList;
-    for (a <- 0 to n){
-      for (b <- 0 to n-a){
-        val c=n-a-b;
-        if(a*10000+b*5000+c*1000==y){
-          return s"${a} ${b} ${c}";
-        }
-      }
-    }
-
-    "-1 -1 -1"
-  }*/
-
   def solve(input:String):String={
     val List(n,y)=input.split(" ").map(_.toInt).toList;
-    val (a,b,c)=f(n,y,(0,0,y/1000));
-
-    s"${a} ${b} ${c}"
+    f1(n,y,0) match{
+      case Some((a,b,c))=>s"${a} ${b} ${c}"
+      case None=>"-1 -1 -1"
+    }
   }
 
-  def f(n:Int,y:Int,v:(Int,Int,Int)):(Int,Int,Int)={
-    val (a,b,c)=v;
-    if (a + b + c == n) {
-        (a,b,c)
-    }else if (a+b+c < n) {
-        (-1,-1,-1)
-    }else if( c >= 5 && n + 4 <= a + b + c) {
-        f(n,y,(a,b+1,c-5))
-    } else if (b >= 2 ){
-        f(n,y,(a+1,b-2,c))
-    } else {
-        (-1,-1,-1)
+  def f1(n:Int,y:Int,a:Int):Option[(Int,Int,Int)]={
+    if(a<=n){
+      f2(n,y,a,0) match{
+        case Some(v)=>Some(v)
+        case None=>f1(n,y,a+1)
+      }
+    }else{
+      None
+    }
+  }
+
+  def f2(n:Int,y:Int,a:Int,b:Int):Option[(Int,Int,Int)]={
+    if(b<=n-a){
+      val c=n-a-b;
+      if(a*10000+b*5000+c*1000==y){
+        Some((a,b,c))
+      }else{
+        f2(n,y,a,b+1)
+      }
+    }else{
+      None
     }
   }
  
