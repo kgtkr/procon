2083049d92150c82ae05eb8e678bbef60316a658
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 17:22:13 +0900

    54

---
+++ abc054_a/Main.hs
@@ -0,0 +1,23 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s | a > b     = "Alice"
+        | b > a     = "Bob"
+        | otherwise = "Draw"
+ where
+  [a, b] = fmap read (words s) :: [Int]
+  x      = if a == 1 then 100 else a
+  y      = if b == 1 then 100 else b
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
