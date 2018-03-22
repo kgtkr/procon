d920941b8c33642f0bc58ea3e29cfe2409a5b739
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 22:59:28 +0900

    abc042_a

---
+++ abc042_a/Main.hs
@@ -0,0 +1,18 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s = if x == [5, 5, 7] then "YES" else "NO"
+  where x = sort $ fmap read (words s) :: [Int]
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
