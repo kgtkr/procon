21286bd190a0e8292fba4b9833e607322e666a18
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 23:16:19 +0900

    format

--- abc039_a/Main.hs
+++ abc039_a/Main.hs
@@ -1,18 +1,18 @@
 import Data.Maybe
 import Data.List
- 
+
 main :: IO ()
 main = do
   input <- getContents
   putStrLn $ trim (solve (trim input))
- 
+
 --処理
 solve :: String -> String
-solve s = show $ (c*b)*2+(a*c)*2+(a*b)*2
-  where [a,b,c] = fmap read (words s) :: [Int]
- 
+solve s = show $ (c * b) * 2 + (a * c) * 2 + (a * b) * 2
+  where [a, b, c] = fmap read (words s) :: [Int]
+
 trimHead :: String -> String
 trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
- 
+
 trim :: String -> String
 trim = reverse . trimHead . reverse . trimHead
