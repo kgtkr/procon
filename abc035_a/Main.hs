ed0cf737536b5fab7e491114697a5f4f40c8f6ee
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 23:26:09 +0900

    abc035_a

---
+++ abc035_a/Main.hs
@@ -0,0 +1,18 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s = if w * 3 == h * 4 then "4:3" else "16:9"
+  where [w, h] = fmap read (words s) :: [Int]
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
