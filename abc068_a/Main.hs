3bf471a5fcb29f1ef0eee89517b1e0837621e2cf
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 16:10:45 +0900

    abc068_a

---
+++ abc068_a/Main.hs
@@ -0,0 +1,17 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve = (++) "ABC"
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
