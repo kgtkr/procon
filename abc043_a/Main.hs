1286475feb3038ad7fa8428be5d7b8b0ec8fff08
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 22:53:48 +0900

    abc043_a

---
+++ abc043_a/Main.hs
@@ -0,0 +1,17 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s = show $ n * (n + 1) `div` 2 where n = read s :: Int
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
