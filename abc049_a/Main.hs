960811b8e588bd003d3f917b044edf5a62bf3898
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 18:17:14 +0900

    abc049_a

---
+++ abc049_a/Main.hs
@@ -0,0 +1,18 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s | s `elem` ["a", "i", "u", "e", "o"] = "vowel"
+        | otherwise                          = "consonant"
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
