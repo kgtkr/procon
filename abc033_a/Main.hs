926a09849326dd079af886185667d19b8a379589
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 23:35:16 +0900

    abc033_a

---
+++ abc033_a/Main.hs
@@ -0,0 +1,17 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s = if (length . nub) s == 1 then "SAME" else "DIFFERENT"
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
