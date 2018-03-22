bec61a4d31449e145a1f5a64564860fc535815cd
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 16:18:09 +0900

    abc066_a

---
+++ abc066_a/Main.hs
@@ -0,0 +1,18 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s = show   $a + b + c - max a (max b c)
+  where [a, b, c] = fmap read (words s) :: [Int]
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
