79898fb2b6a5b0c4148c3a57514dfeb3f437f2a2
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 16:44:58 +0900

    abc059_a

---
+++ abc059_a/Main.hs
@@ -0,0 +1,18 @@
+import Data.Maybe
+import Data.List
+import Data.Char
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve = fmap (toUpper . head) . words
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
