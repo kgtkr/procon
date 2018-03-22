8780071db30d60112e404a2e4d510cf33ed1061e
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 17:30:22 +0900

    053

---
+++ abc053_a/Main.hs
@@ -0,0 +1,19 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve s | x < 1200  = "ABC"
+        | otherwise = "ARC"
+  where x = read s :: Int
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
