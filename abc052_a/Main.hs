fa72810bd23113bba4955093ac5fb75f9d28644d
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 17:34:29 +0900

    ミス

--- abc052_a/Main.hs
+++ abc052_a/Main.hs
@@ -1,19 +1,22 @@
 import Data.Maybe
 import Data.List
- 
+
 main :: IO ()
 main = do
   input <- getContents
   putStrLn $ trim (solve (trim input))
- 
+
 --処理
 solve :: String -> String
-solve s | x < 1200  = "ABC"
-        | otherwise = "ARC"
-  where x = read s :: Int
- 
+solve s | x > y     = show x
+        | otherwise = show y
+ where
+  [a, b, c, d] = fmap read (words s) :: [Int]
+  x            = a * b
+  y            = c * d
+
 trimHead :: String -> String
 trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
- 
+
 trim :: String -> String
 trim = reverse . trimHead . reverse . trimHead
