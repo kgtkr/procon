aa5a7010acbacff73df599f30c4ff958c57a9d32
kgtkr <kgtkr.jp@gmail.com>
2018-03-22 17:38:59 +0900

    abc051_a

---
+++ abc051_a/Main.hs
@@ -0,0 +1,21 @@
+import Data.Maybe
+import Data.List
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ trim (solve (trim input))
+
+--処理
+solve :: String -> String
+solve = fmap f
+
+f :: Char -> Char
+f ',' = ' '
+f s   = s
+
+trimHead :: String -> String
+trimHead = dropWhile (\s -> isJust (elemIndex s [' ', '\t', '\n', '\r']))
+
+trim :: String -> String
+trim = reverse . trimHead . reverse . trimHead
