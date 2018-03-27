350c92c9e7a764112ac7c526bb73307143e0cc92
kgtkr <kgtkr.jp@gmail.com>
2018-03-27 12:35:02 +0900

    テンプレ修正

--- _template/src/main.rs
+++ _template/src/main.rs
@@ -2,6 +2,71 @@ extern crate core;
 
 use std::io::{self, Read};
 
+#[macro_use]
+mod parser {
+    macro_rules! input {
+    ($s:expr=>$($t:tt)*) => {
+        let mut lines=$s.split("\n");
+        $(
+            line_parse!(lines,$t);
+        )*
+    };
+    }
+
+    macro_rules! line_parse {
+    ($lines:expr,($($name:ident:$t:tt)*)) => {
+        let mut line=$lines.next().unwrap().split_whitespace();
+        $(value_def!(line,$name,$t);)*
+    };
+
+    //複数行
+    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+        values_def!($lines,$n,$name,$t);
+    };
+    }
+
+    macro_rules! value_def {
+    ($line:expr,$name:ident,$t:tt) => {
+        let $name=value!($line,$t);
+    };
+    }
+
+    macro_rules! values_def {
+    ($lines:expr,$n:expr,$name:ident,$t:tt) => {
+        let $name={
+        let mut vec=Vec::new();
+        for i in 0..$n{
+            let mut next=$lines.next().unwrap().split_whitespace();
+            vec.push(value!(next,$t));
+        }
+        vec
+        };
+    };
+    }
+
+    macro_rules! value {
+    //配列
+    ($line:expr,[$t:tt]) => {
+        $line.map(|x|{
+        let mut iter=::std::iter::once(x);
+        value!(iter,$t)
+        }).collect::<Vec<_>>()
+    };
+    //タプル
+    ($line:expr,($($t:tt),*)) => {
+        ($(value!($line,$t),)*)
+    };
+    //文字列
+    ($line:expr,#) => {
+        $line.next().unwrap()
+    };
+    //単一値
+    ($line:expr,$t:ty) => {
+        $line.next().unwrap().parse::<$t>().unwrap()
+    };
+    }
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -10,11 +75,8 @@ fn main() {
 }
 
 fn run(input: String) -> String {
-    let list = input
-        .split_whitespace()
-        .map(|x| x.parse::<i64>().unwrap())
-        .collect::<Vec<_>>();
-    let n = list[0] + list[1];
+    input!(input=>(a:i64 b:i64));
+    let n = a + b;
     n.to_string()
 }
 
