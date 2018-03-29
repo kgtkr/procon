163e770241ec14292f35dec27225d3e596ecef3d
kgtkr <kgtkr.jp@gmail.com>
2018-03-29 16:32:27 +0900

    B無理

---
+++ _aoj/RitsCamp18Day3/B/src/main.rs
@@ -0,0 +1,110 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_use]
+mod parser {
+    macro_rules! input {
+    ($s:expr=>$($t:tt)*) => {
+        let mut lines=$s.split("\n");
+        $(
+            line_parse!(lines,$t);
+        )*
+    };
+    }
+
+    macro_rules! line_parse {
+    ($lines:expr,($($name:ident:$t:tt)*)) => {
+        let mut line=$lines.next().unwrap().split_whitespace();
+        $(value_def!(line,$name,$t);)*
+    };
+
+    //複数行
+    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+        values_def!($lines,$n,$name,$t);
+    };
+    }
+
+    macro_rules! value_def {
+    ($line:expr,$name:ident,$t:tt) => {
+        let $name=value!($line,$t);
+    };
+    }
+
+    macro_rules! values_def {
+    ($lines:expr,$n:expr,$name:ident,$t:tt) => {
+        let $name={
+        let mut vec=Vec::new();
+        for i in 0..$n{
+            let mut next=$lines.next().unwrap().split_whitespace();
+            vec.push(value!(next,$t));
+        }
+        vec
+        };
+    };
+    }
+
+    macro_rules! value {
+    //配列
+    ($line:expr,[$t:tt]) => {
+        $line.map(|x|{
+        let mut iter=::std::iter::once(x);
+        value!(iter,$t)
+        }).collect::<Vec<_>>()
+    };
+    //タプル
+    ($line:expr,($($t:tt),*)) => {
+        ($(value!($line,$t),)*)
+    };
+    //文字列
+    ($line:expr,#) => {
+        $line.next().unwrap()
+    };
+    //単一値
+    ($line:expr,$t:ty) => {
+        $line.next().unwrap().parse::<$t>().unwrap()
+    };
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = run(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn run(input: String) -> String {
+    input!(input=>(n:usize)(a:[i64]));
+    let mut dp = Vec::new();
+
+    let mut res = 0;
+    for i in 0..n {
+        dp.push(1);
+        for j in 0..i {
+            if a[j] < a[i] {
+                dp[i] = std::cmp::max(dp[i], dp[i] * a[j]);
+            }
+            res = std::cmp::max(res, dp[i]);
+        }
+    }
+    res.to_string()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!(super::run($input.to_string()), $output.to_string());
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "4
+2 0 -2 1" => "2",
+}
