f8f68acc8d9a73fb3accef9732c9a935c35fecd2
tkr <kgtkr.jp@gmail.com>
2018-04-07 20:58:33 +0900

    駄目

---
+++ arc071_a/src/main.rs
@@ -0,0 +1,126 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_use]
+mod parser {
+    macro_rules! input {
+    ($s:expr=>$($t:tt)*) => {
+        let mut lines=$s.split("\n");
+        $(
+            line_parse!(lines,$t);
+        )*
+    };
+    }
+
+    macro_rules! line_parse {
+    ($lines:expr,($($name:ident:$t:tt)*)) => {
+        let mut line=$lines.next().unwrap().split_whitespace();
+        $(value_def!(line,$name,$t);)*
+    };
+
+    //複数行
+    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+        values_def!($lines,$n,$name,$t);
+    };
+    }
+
+    macro_rules! value_def {
+    ($line:expr,$name:ident,$t:tt) => {
+        let $name=value!($line,$t);
+    };
+    }
+
+    macro_rules! values_def {
+    ($lines:expr,$n:expr,$name:ident,$t:tt) => {
+        let $name={
+        let mut vec=Vec::new();
+        for i in 0..$n{
+            let mut next=$lines.next().unwrap().split_whitespace();
+            vec.push(value!(next,$t));
+        }
+        vec
+        };
+    };
+    }
+
+    macro_rules! value {
+    //配列
+    ($line:expr,[$t:tt]) => {
+        $line.map(|x|{
+        let mut iter=::std::iter::once(x);
+        value!(iter,$t)
+        }).collect::<Vec<_>>()
+    };
+    //タプル
+    ($line:expr,($($t:tt),*)) => {
+        ($(value!($line,$t),)*)
+    };
+    //文字列
+    ($line:expr,#) => {
+        $line.next().unwrap()
+    };
+    //単一値
+    ($line:expr,$t:ty) => {
+        $line.next().unwrap().parse::<$t>().unwrap()
+    };
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize){n;list:#});
+    let mut list = list.into_iter()
+        .map(|x| {
+            let mut v = x.chars().collect::<Vec<_>>();
+            v.sort();
+            v
+        })
+        .collect::<Vec<_>>();
+
+    let mut res = Vec::new();
+    for c in 'a' as u8..('z' as u8 + 1) {
+        let c = c as char;
+
+        while list.clone().into_iter().any(|x| x.len() != 0 && x[0] == c) {
+            if list.clone().into_iter().all(|x| x[0] == c) {
+                for i in 0..list.len() {
+                    list[i].remove(0);
+                }
+                res.push(c);
+            } else {
+                for i in 0..list.len() {
+                    if list[i][0] == c {
+                        list[i].remove(0);
+                    }
+                }
+            }
+        }
+    }
+
+    res.into_iter().collect::<String>()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "3\ncbaa\ndaacc\nacacac" => "aac",
+    test2: "3\na\naa\nb" => "",
+}
