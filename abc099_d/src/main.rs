241337a21410690c18f8ce23c68069f7a273aef7
tkr <kgtkr.jp@gmail.com>
2018-06-10 23:13:06 +0900

    d

--- abc099_d/src/main.rs
+++ abc099_d/src/main.rs
@@ -75,9 +75,54 @@ fn main() {
 }
 
 fn solve(input: String) -> String {
-    input!(input=>(a:i64 b:i64));
-    let n = a + b;
-    n.to_string()
+    input!(input=>(n:usize c:usize){c;d_list:[i64]}{n;c_list:[usize]});
+    //1列目が色iの時の違和感の合計
+    let d1 = (0..c)
+        .map(|c| {
+            c_list
+                .clone()
+                .into_iter()
+                .map(|x| x.into_iter().map(|x| d_list[x - 1][c]).collect::<Vec<_>>())
+                .collect::<Vec<_>>()
+        })
+        .map(|list| {
+            let mut sum_0 = 0;
+            let mut sum_1 = 0;
+            let mut sum_2 = 0;
+            for i in 0..n {
+                for j in 0..n {
+                    match (i + j + 2) % 3 {
+                        0 => {
+                            sum_0 += list[i][j];
+                        }
+                        1 => {
+                            sum_1 += list[i][j];
+                        }
+                        2 => {
+                            sum_2 += list[i][j];
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            (sum_0, sum_1, sum_2)
+        })
+        .collect::<Vec<_>>();
+
+    let mut min = 10000000000;
+    for a in 0..c {
+        for b in 0..c {
+            for c in 0..c {
+                if a != b && a != c && b != c {
+                    let x = d1[a].0 + d1[b].1 + d1[c].2;
+                    if x < min {
+                        min = x;
+                    }
+                }
+            }
+        }
+    }
+    min.to_string()
 }
 
 macro_rules! tests {
