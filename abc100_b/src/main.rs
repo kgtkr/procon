e5fd48e7f7bbf2a4076037b7a44eb1422dd1fb4d
tkr <kgtkr.jp@gmail.com>
2018-06-16 21:05:41 +0900

    b

--- abc100_b/src/main.rs
+++ abc100_b/src/main.rs
@@ -26,22 +26,22 @@ mod parser {
     }
 
     macro_rules! value_def {
-    ($line:expr,$name:ident,$t:tt) => {
-        let $name=value!($line,$t);
-    };
+        ($line:expr, $name:ident, $t:tt) => {
+            let $name = value!($line, $t);
+        };
     }
 
     macro_rules! values_def {
-    ($lines:expr,$n:expr,$name:ident,$t:tt) => {
-        let $name={
-        let mut vec=Vec::new();
-        for i in 0..$n{
-            let mut next=$lines.next().unwrap().split_whitespace();
-            vec.push(value!(next,$t));
-        }
-        vec
+        ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+            let $name = {
+                let mut vec = Vec::new();
+                for i in 0..$n {
+                    let mut next = $lines.next().unwrap().split_whitespace();
+                    vec.push(value!(next, $t));
+                }
+                vec
+            };
         };
-    };
     }
 
     macro_rules! value {
@@ -75,9 +75,14 @@ fn main() {
 }
 
 fn solve(input: String) -> String {
-    input!(input=>(a:i64 b:i64));
-    let n = a + b;
-    n.to_string()
+    input!(input=>(d:i64 n:i64));
+    if d == 0 {
+        n
+    } else if d == 1 {
+        100 * n
+    } else {
+        10000 * n
+    }.to_string()
 }
 
 macro_rules! tests {
@@ -94,9 +99,7 @@ macro_rules! tests {
 }
 
 tests! {
-    test1: "3 9" => "12",
-    test2: "31 32" => "63",
-    test3: "1 2" => "3",
-    test4: "-1 2" => "1",
-    test5: "10 1" => "11",
+    test1: "0 5" => "5",
+    test2: "1 11" => "1100",
+    test3: "2 85" => "850000",
 }
