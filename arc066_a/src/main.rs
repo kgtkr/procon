09ab943df73ef8e304bf2bd9c4abff40259aad1d
tkr <kgtkr.jp@gmail.com>
2018-07-02 22:52:42 +0900

    c

---
+++ arc066_a/src/main.rs
@@ -0,0 +1,126 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_use]
+mod parser {
+    macro_rules! input {
+    ($s:expr=>$($t:tt)*) => {
+        let mut lines=$s.split("\n");
+        $(
+            line_parse!(lines,$t);
+        )*
+    };
+    }
+
+    macro_rules! line_parse {
+    ($lines:expr,($($name:ident:$t:tt)*)) => {
+        let mut line=$lines.next().unwrap().split_whitespace();
+        $(value_def!(line,$name,$t);)*
+    };
+
+    //複数行
+    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+        values_def!($lines,$n,$name,$t);
+    };
+    }
+
+    macro_rules! value_def {
+        ($line:expr, $name:ident, $t:tt) => {
+            let $name = value!($line, $t);
+        };
+    }
+
+    macro_rules! values_def {
+        ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+            let $name = {
+                let mut vec = Vec::new();
+                for i in 0..$n {
+                    let mut next = $lines.next().unwrap().split_whitespace();
+                    vec.push(value!(next, $t));
+                }
+                vec
+            };
+        };
+    }
+
+    macro_rules! value {
+    //配列
+    ($line:expr,[$t:tt]) => {
+        $line.map(|x|{
+        let mut iter=::std::iter::once(x);
+        value!(iter,$t)
+        }).collect::<Vec<_>>()
+    };
+    //タプル
+    ($line:expr,($($t:tt),*)) => {
+        ($(value!($line,$t),)*)
+    };
+    //文字列
+    ($line:expr,#) => {
+        $line.next().unwrap()
+    };
+    //単一値
+    ($line:expr,$t:ty) => {
+        $line.next().unwrap().parse::<$t>().unwrap()
+    };
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize)(list:[usize]));
+    let mut vec = Vec::<usize>::with_capacity(n);
+    vec.resize(n, 0);
+    for x in list {
+        vec[x] += 1;
+    }
+    if vec[0] > 1 || (n % 2 == 0 && vec[0] != 0) {
+        0
+    } else {
+        let mut res = 1;
+        for x in vec.into_iter().skip(1) {
+            res = mul(
+                res,
+                if x == 2 {
+                    2
+                } else if x == 0 {
+                    1
+                } else {
+                    0
+                },
+            );
+        }
+        res
+    }.to_string()
+}
+
+fn mul(a: usize, b: usize) -> usize {
+    let g = 1000000007;
+    ((a % g) * (b % g)) % g
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "5\n2 4 4 0 2" => "4",
+    test2: "7\n6 4 0 2 4 0 2" => "0",
+    test3: "8\n7 5 1 1 7 3 5 3" => "16",
+}
