3fcb0d28ac6d6f5e68ab4302cc28c51c484a17a3
tkr <kgtkr.jp@gmail.com>
2018-07-08 22:10:21 +0900

    arc061_a

---
+++ arc061_a/src/main.rs
@@ -0,0 +1,122 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_use]
+mod parser {
+    macro_rules! input {
+    ($s:expr=>$($t:tt)*) => {
+        let mut lines=$s.split("\n");
+        $(
+            line_parse!(lines,$t);
+        )*
+    };
+    }
+
+    macro_rules! line_parse {
+    ($lines:expr,($($name:ident:$t:tt)*)) => {
+        let mut line=$lines.next().unwrap().split_whitespace();
+        $(value_def!(line,$name,$t);)*
+    };
+
+    //複数行
+    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+        values_def!($lines,$n,$name,$t);
+    };
+    }
+
+    macro_rules! value_def {
+        ($line:expr, $name:ident, $t:tt) => {
+            let $name = value!($line, $t);
+        };
+    }
+
+    macro_rules! values_def {
+        ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+            let $name = {
+                let mut vec = Vec::new();
+                for i in 0..$n {
+                    let mut next = $lines.next().unwrap().split_whitespace();
+                    vec.push(value!(next, $t));
+                }
+                vec
+            };
+        };
+    }
+
+    macro_rules! value {
+    //配列
+    ($line:expr,[$t:tt]) => {
+        $line.map(|x|{
+        let mut iter=::std::iter::once(x);
+        value!(iter,$t)
+        }).collect::<Vec<_>>()
+    };
+    //タプル
+    ($line:expr,($($t:tt),*)) => {
+        ($(value!($line,$t),)*)
+    };
+    //文字列
+    ($line:expr,#) => {
+        $line.next().unwrap()
+    };
+    //単一値
+    ($line:expr,$t:ty) => {
+        $line.next().unwrap().parse::<$t>().unwrap()
+    };
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    f(input, String::new(), 0).to_string()
+}
+
+fn f(s1: String, s2: String, sum: i64) -> i64 {
+    if s2.len() == 0 {
+        let s2 = s1.clone().chars().take(1).collect::<String>();
+        let s1 = s1.clone().chars().skip(1).collect::<String>();
+        f(s1, s2, sum)
+    } else if s1.len() == 0 {
+        sum + s2.parse::<i64>().unwrap()
+    } else {
+        //s2を使う
+        let sum1 = {
+            let sum = sum + s2.parse::<i64>().unwrap();
+            let s2 = String::new();
+            f(s1.clone(), s2, sum)
+        };
+        //s2を使わない
+        let sum2 = {
+            let s2 =
+                format!("{}{}", s2, s1.clone().chars().take(1).collect::<String>()).to_string();
+            let s1 = s1.clone().chars().skip(1).collect::<String>();
+            f(s1, s2, sum)
+        };
+        sum1 + sum2
+    }
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "125" => "176",
+    test2: "9999999999" => "12656242944",
+}
