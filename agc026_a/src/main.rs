afed1c8c747df48863e8522628df088645e26fdf
kgtkr <kgtkr.jp@gmail.com>
2018-07-16 18:37:41 +0900

    agc026_a

---
+++ agc026_a/src/main.rs
@@ -0,0 +1,110 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_use]
+mod parser {
+    macro_rules! input {
+    ($s:expr=>$($t:tt)*) => {
+        let mut lines=$s.split("\n");
+        $(
+            line_parse!(lines,$t);
+        )*
+    };
+    }
+
+    macro_rules! line_parse {
+    ($lines:expr,($($name:ident:$t:tt)*)) => {
+        let mut line=$lines.next().unwrap().split_whitespace();
+        $(value_def!(line,$name,$t);)*
+    };
+
+    //複数行
+    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+        values_def!($lines,$n,$name,$t);
+    };
+    }
+
+    macro_rules! value_def {
+        ($line:expr, $name:ident, $t:tt) => {
+            let $name = value!($line, $t);
+        };
+    }
+
+    macro_rules! values_def {
+        ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+            let $name = {
+                let mut vec = Vec::new();
+                for i in 0..$n {
+                    let mut next = $lines.next().unwrap().split_whitespace();
+                    vec.push(value!(next, $t));
+                }
+                vec
+            };
+        };
+    }
+
+    macro_rules! value {
+    //配列
+    ($line:expr,[$t:tt]) => {
+        $line.map(|x|{
+        let mut iter=::std::iter::once(x);
+        value!(iter,$t)
+        }).collect::<Vec<_>>()
+    };
+    //タプル
+    ($line:expr,($($t:tt),*)) => {
+        ($(value!($line,$t),)*)
+    };
+    //文字列
+    ($line:expr,#) => {
+        $line.next().unwrap()
+    };
+    //単一値
+    ($line:expr,$t:ty) => {
+        $line.next().unwrap().parse::<$t>().unwrap()
+    };
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize)(list:[i64]));
+    let mut list = list;
+    let mut count = 0;
+    let mut c = 10000000;
+    for i in 1..n {
+        if list[i - 1] == list[i] {
+            list[i] = c;
+            c += 1;
+            count += 1;
+        }
+    }
+    count.to_string()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "5\n1 1 2 2 2" => "2",
+    test2: "3\n1 2 1" => "0",
+    test3: "5\n1 1 1 1 1" => "2",
+    test4: "14\n1 2 2 3 3 3 4 4 4 4 1 2 3 4" => "4",
+}
