d4fe1310d575921627fbeb5284a0cb3fcf6733be
kgtkr <kgtkr.jp@gmail.com>
2018-08-11 23:49:02 +0900

    改善

--- abc105-d-hs/app/Main.hs
+++ abc105-d-hs/app/Main.hs
@@ -8,5 +8,5 @@ main = do
     let mlist             = (map (`mod` m) . scanl1 (+)) list
     let zero              = (length . filter (== 0)) mlist
     let f n = (n * (n - 1)) `div` 2
-    let x = zero + (sum . map f . map length . group . sort) mlist
+    let x = ((+ zero) . sum . map f . map length . group . sort) mlist
     putStrLn $ show x
