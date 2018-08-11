42f3b4806b2dbbe0ec55dad3c9b20fc3faca7148
kgtkr <kgtkr.jp@gmail.com>
2018-08-11 20:56:24 +0900

    マクロ修正

--- _template/src/main.rs
+++ _template/src/main.rs
@@ -2,69 +2,71 @@ extern crate core;
 
 use std::io::{self, Read};
 
-#[macro_use]
-mod parser {
-    macro_rules! input {
-    ($s:expr=>$($t:tt)*) => {
-        let mut lines=$s.split("\n");
-        $(
-            line_parse!(lines,$t);
-        )*
-    };
-    }
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
 
-    macro_rules! line_parse {
-    ($lines:expr,($($name:ident:$t:tt)*)) => {
-        let mut line=$lines.next().unwrap().split_whitespace();
-        $(value_def!(line,$name,$t);)*
-    };
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
 
-    //複数行
-    ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
-        values_def!($lines,$n,$name,$t);
-    };
-    }
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
 
-    macro_rules! value_def {
-    ($line:expr,$name:ident,$t:tt) => {
-        let $name=value!($line,$t);
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
     };
-    }
+}
 
-    macro_rules! values_def {
-    ($lines:expr,$n:expr,$name:ident,$t:tt) => {
-        let $name={
-        let mut vec=Vec::new();
-        for i in 0..$n{
-            let mut next=$lines.next().unwrap().split_whitespace();
-            vec.push(value!(next,$t));
-        }
-        vec
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
         };
     };
-    }
+}
 
-    macro_rules! value {
-    //配列
-    ($line:expr,[$t:tt]) => {
-        $line.map(|x|{
-        let mut iter=::std::iter::once(x);
-        value!(iter,$t)
-        }).collect::<Vec<_>>()
-    };
-    //タプル
-    ($line:expr,($($t:tt),*)) => {
-        ($(value!($line,$t),)*)
-    };
-    //文字列
-    ($line:expr,#) => {
-        $line.next().unwrap()
-    };
-    //単一値
-    ($line:expr,$t:ty) => {
-        $line.next().unwrap().parse::<$t>().unwrap()
-    };
-    }
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
 }
 
 fn main() {
