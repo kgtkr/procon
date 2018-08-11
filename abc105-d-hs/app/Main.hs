2986f6ac83cbe70a327b611837d40b2e4d6a1dcf
kgtkr <kgtkr.jp@gmail.com>
2018-08-11 23:51:09 +0900

    改善

--- abc105-d-hs/app/Main.hs
+++ abc105-d-hs/app/Main.hs
@@ -5,8 +5,12 @@ main :: IO ()
 main = do
     input <- getContents
     let [n, m] : list : _ = map read . words <$> lines input :: [[Int]]
-    let mlist             = (map (`mod` m) . scanl1 (+)) list
-    let zero              = (length . filter (== 0)) mlist
     let f n = (n * (n - 1)) `div` 2
-    let x = ((+ zero) . sum . map f . map length . group . sort) mlist
+    let
+        x =
+            (sum . map f . map length . group . sort . map (`mod` m) . scanl
+                    (+)
+                    0
+                )
+                list
     putStrLn $ show x
