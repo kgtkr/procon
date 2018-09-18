909425b1c38d463c09b065f0b66610a956b08262
kgtkr <kgtkr.jp@gmail.com>
2018-09-18 22:05:29 +0900

    c無理

---
+++ arc102-a-hs/app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+main :: IO ()
+main = do
+    [n, k] <- (map read . words) <$> getLine :: IO [Int]
+    print $ if k == 2
+        then (n `div` 2) ^ 3 + (n `myDiv` 2) ^ 3
+        else (n `div` k) ^ 3 + if even k then (n `myDiv` k) ^ 3 else 0
+    return ()
+
+myDiv a b = (a + (b - 1)) `div` b
