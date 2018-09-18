dd449df5034ea6e90bb40dbd788c451c3f668c35
kgtkr <kgtkr.jp@gmail.com>
2018-09-18 22:09:39 +0900

    C

--- arc102-a-hs/app/Main.hs
+++ arc102-a-hs/app/Main.hs
@@ -5,7 +5,9 @@ main = do
     [n, k] <- (map read . words) <$> getLine :: IO [Int]
     print $ if k == 2
         then (n `div` 2) ^ 3 + (n `myDiv` 2) ^ 3
-        else (n `div` k) ^ 3 + if even k then (n `myDiv` k) ^ 3 else 0
+        else (n `div` k) ^ 3 + if even k
+            then ((n + k `div` 2) `div` k) ^ 3
+            else 0
     return ()
 
 myDiv a b = (a + (b - 1)) `div` b
