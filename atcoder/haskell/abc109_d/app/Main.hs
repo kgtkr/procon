459ab68e5a5a1c64332a36fd7e54a9af3f8f888a
kgtkr <kgtkr.jp@gmail.com>
2018-09-19 13:27:37 +0900

    綺麗に

--- atcoder/haskell/abc109_d/app/Main.hs
+++ atcoder/haskell/abc109_d/app/Main.hs
@@ -1,13 +1,12 @@
 module Main where
 
 import           Data.List
+import           Control.Monad
 
 main :: IO ()
 main = do
     [h, w] <- (map read . words) <$> getLine :: IO [Int]
-    list   <-
-        (map (map read . words)) <$> (sequence . take h . repeat) getLine :: IO
-            [[Int]]
+    list   <- map (map read . words) <$> (replicateM h) getLine :: IO [[Int]]
     let res1    = (map goRight . enumerate2d) list
     let list2   = (map snd) res1
     let result1 = (concatMap fst) res1
