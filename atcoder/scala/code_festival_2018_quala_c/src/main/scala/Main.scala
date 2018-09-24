1e8e68329424ab83f179a55991f3913c27f66d3d
kgtkr <kgtkr.jp@gmail.com>
2018-09-24 14:46:26 +0900

    無理

--- atcoder/scala/code_festival_2018_quala_c/src/main/scala/Main.scala
+++ atcoder/scala/code_festival_2018_quala_c/src/main/scala/Main.scala
@@ -1,12 +1,20 @@
 import scala.io.StdIn._
 
 object Main extends App {
-  val Array(n,k)=readLine().split(" ").map(_.toLong)
+  val Array(n,k1)=readLine().split(" ").map(_.toLong)
   val list=readLine().split(" ").map(_.toLong)
   //パターン数
   //パターン数-1回割ると0になる
-  val patternCount=list.map(x=>(0l until 100).scan(x)((x, _) => x/2).takeWhile(_!=0).size.toLong+1).sorted.toList
+  val patternCount=list.map(x=>(0l until 100).scan(x)((x, _) => x/2).takeWhile(_!=0).size.toLong+1).sorted.toArray
 
   //kが(パターン数-1).sumより大きくても意味ないので
-  val k2=k min patternCount.map(_-1).sum
+  val k=k1 min patternCount.map(_-1).sum
+
+  //現在の操作回数をcountとしたときのi番目以降のパターン数
+  def f(i:Long,count:Long)=Long{
+    val remain=k-count
+
+    (0 to remain).map(x=>f(i+1,count+x))
+    1l
+  }
 }
