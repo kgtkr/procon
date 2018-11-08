db3bc8325808d29a6ccf192f3d6965fa5c4be2d0
kgtkr <kgtkr.jp@gmail.com>
2018-11-08 19:48:40 +0900

    無理

--- atcoder/haskell/abc032_d/app/Main.hs
+++ atcoder/haskell/abc032_d/app/Main.hs
@@ -8,13 +8,25 @@ main :: IO ()
 main = do
   (n, w) <- readPair
   list   <- replicateM n readPair
-  print $ f w 0 list
-  return ()
+  print $ napsack w list
  where
   readPair = listToTuple2 . fmap read . words <$> getLine
   listToTuple2 [x, y] = (x, y)
-  f r i ((v, w) : xs)
-    | r < w     = f r (i + 1) xs
-    | otherwise = max ((f (r - w) (i + 1) xs) + v) (f r (i + 1) xs)
-  f _ _ [] = 0
-
+  napsack r list = runST $ do
+    arr <-
+      newArray ((0, 0), (100, 10000)) (-1) :: ST s (STArray s (Int, Int) Int)
+    f arr 0 r list
+  f arr i r ((v, w) : xs) = do
+    x <- readArray arr (i, r)
+    if x /= (-1)
+      then return x
+      else do
+        res <- if r < w
+          then f arr (i + 1) r xs
+          else
+            max
+            <$> ((+ v) <$> (f arr (i + 1) (r - w) xs))
+            <*> (f arr (i + 1) r xs)
+        writeArray arr (i, r) res
+        return res
+  f arr _ _ [] = return 0
