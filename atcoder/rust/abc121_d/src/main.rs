8f32f9f884ede0dc636e5c765a5b59fd875de5a9
kgtkr <kgtkr.jp@gmail.com>
2019-03-09 21:54:41 +0900

    d

--- atcoder/rust/abc121_d/src/main.rs
+++ atcoder/rust/abc121_d/src/main.rs
@@ -25,22 +25,22 @@ macro_rules! line_parse {
 }
 
 macro_rules! value_def {
-    ($line:expr, $name:ident, $t:tt) => {
-        let $name = value!($line, $t);
-    };
+  ($line:expr, $name:ident, $t:tt) => {
+    let $name = value!($line, $t);
+  };
 }
 
 macro_rules! values_def {
-    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
-        let $name = {
-            let mut vec = Vec::new();
-            for i in 0..$n {
-                let mut next = $lines.next().unwrap().split_whitespace();
-                vec.push(value!(next, $t));
-            }
-            vec
-        };
+  ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+    let $name = {
+      let mut vec = Vec::new();
+      for i in 0..$n {
+        let mut next = $lines.next().unwrap().split_whitespace();
+        vec.push(value!(next, $t));
+      }
+      vec
     };
+  };
 }
 
 macro_rules! value {
@@ -70,16 +70,41 @@ macro_rules! value {
 }
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let output = solve(input.trim().to_string());
-    println!("{}", output);
+  let mut input = String::new();
+  io::stdin().read_to_string(&mut input).unwrap();
+  let output = solve(input.trim().to_string());
+  println!("{}", output);
 }
 
 fn solve(input: String) -> String {
-    input!(input=>(a:i64 b:i64));
-    let n = a + b;
-    n.to_string()
+  input!(input=>(a:i64 b:i64));
+  //41桁くらい
+  let mut arr = Vec::new();
+  arr.resize(41, 0);
+  for i in 0..arr.len() {
+    arr[i] = xn(b, (i + 1) as u32) - xn(a - 1, (i + 1) as u32);
+  }
+
+  let mut p = 1;
+  let mut res = 0;
+  for &x in &arr {
+    res += p * (x % 2);
+    p *= 2;
+  }
+  res.to_string()
+}
+
+fn xn(x: i64, n: u32) -> i64 {
+  let p = 2i64.pow(n);
+  (x + 1) / p * (p / 2) + std::cmp::max(0, (x + 1) % p - p / 2)
+}
+
+#[test]
+fn xn_test() {
+  assert_eq!(xn(1, 1), 1);
+  assert_eq!(xn(0, 1), 0);
+  assert_eq!(xn(6, 3), 3);
+  assert_eq!(xn(4, 2), 2);
 }
 
 macro_rules! tests {
@@ -96,9 +121,7 @@ macro_rules! tests {
 }
 
 tests! {
-    test1: "3 9" => "12",
-    test2: "31 32" => "63",
-    test3: "1 2" => "3",
-    test4: "-1 2" => "1",
-    test5: "10 1" => "11",
+    test1: "2 4\n" => "5\n",
+    test2: "123 456\n" => "435\n",
+    test3: "123456789012 123456789012\n" => "123456789012\n",
 }
