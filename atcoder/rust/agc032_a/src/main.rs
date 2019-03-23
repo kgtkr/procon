3d9d1c839d8d2ff1e30ee3c9db856c4017adec30
kgtkr <kgtkr.jp@gmail.com>
2019-03-23 22:29:24 +0900

    a

---
+++ atcoder/rust/agc032_a/src/main.rs
@@ -0,0 +1,125 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
+
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
+
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
+
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
+    };
+}
+
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
+        };
+    };
+}
+
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize)(list:[i64]));
+    let mut list = list.into_iter().map(Some).collect::<Vec<_>>();
+    let mut res = Vec::new();
+    //後ろから
+    for _ in 0..n {
+        for i in 0..n {
+            let i = n - i - 1;
+            //前の個数
+            let mut count = 0;
+            for i in 0..i {
+                if list[i].is_none() {
+                    count += 1;
+                }
+            }
+            if Some(count + 1) == list[i] {
+                res.push(list[i].unwrap().to_string());
+                list[i] = None;
+                break;
+            }
+        }
+    }
+    if res.len() == n {
+        res.join("\n")
+    } else {
+        "-1".to_string()
+    }
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "3\n1 2 1\n" => "1\n1\n2\n",
+    test2: "2\n2 2\n" => "-1\n",
+    test3: "9\n1 1 1 2 2 1 2 3 2\n" => "1\n2\n2\n3\n1\n2\n2\n1\n1\n",
+}
