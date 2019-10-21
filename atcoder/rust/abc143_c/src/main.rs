40d5d29bb0611ed2701cb86b59be5645b581a959
kgtkr <kgtkr.jp@gmail.com>
2019-10-21 21:53:37 +0900

    abc143_c

---
+++ atcoder/rust/abc143_c/src/main.rs
@@ -0,0 +1,109 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
+
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
+
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
+
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
+    };
+}
+
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
+        };
+    };
+}
+
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize)(s:#));
+    let mut old = ' ';
+    let mut res = 0;
+    for c in s.chars() {
+        if c != old {
+            old = c;
+            res += 1;
+        }
+    }
+    res.to_string()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "10\naabbbbaaca\n" => "5\n",
+    test2: "5\naaaaa\n" => "1\n",
+    test3: "20\nxxzaffeeeeddfkkkkllq\n" => "10\n",
+}
