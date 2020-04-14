fa33a8c137421a99afc77999eec5d4aaca4feb16
kgtkr <kgtkr.jp@gmail.com>
2020-04-15 00:41:16 +0900

    リファクタリング

--- atcoder/ocaml/arc097_b/main.ml
+++ atcoder/ocaml/arc097_b/main.ml
@@ -1,62 +1,77 @@
-type uf_node = {
-  mutable par: int;
-  mutable rank: int;
-}
-;;
+module UF = struct
+  type node = {
+    mutable par: int;
+    mutable rank: int;
+  };;
 
-let uf_make n = Array.init n (fun i -> { par = i; rank = 0 })
-;;
 
-(* 根を求める *)
-let rec uf_find uf x =
-  if uf.(x).par == x
-  then x
-  else
-    let par = uf.(x).par in
-    let v = uf_find uf par in
-    uf.(x).par <- v;
-    v
-;;
+  let make n = Array.init n (fun i -> { par = i; rank = 0 })
+  ;;
 
-(* xとyの集合を併合 *)
-let uf_unite uf x y =
-  let x = uf_find uf x in
-  let y = uf_find uf y in
-  if x != y then
-    if uf.(x).rank < uf.(y).rank
-    then (
-      uf.(x).par <- y;
-      ()
-    )
-    else (
-      uf.(y).par <- x;
-      if uf.(x).rank == uf.(y).rank then (
-        uf.(x).rank <- uf.(x).rank + 1;
+  (* 根を求める *)
+  let rec find uf x =
+    if uf.(x).par == x
+    then x
+    else
+      let par = uf.(x).par in
+      let v = find uf par in
+      uf.(x).par <- v;
+      v
+  ;;
+
+  (* xとyの集合を併合 *)
+  let unite uf x y =
+    let x = find uf x in
+    let y = find uf y in
+    if x != y then
+      if uf.(x).rank < uf.(y).rank
+      then (
+        uf.(x).par <- y;
         ()
       )
-    )
+      else (
+        uf.(y).par <- x;
+        if uf.(x).rank == uf.(y).rank then (
+          uf.(x).rank <- uf.(x).rank + 1;
+          ()
+        )
+      )
+  ;;
+
+  let some uf x y = find uf x == find uf y
+  ;;
+end
+
+let dec x = x -1
 ;;
 
-let uf_some uf x y = uf_find uf x == uf_find uf y
+let tuple2map fa fb (a, b) = (fa a, fb b)
 ;;
 
-let rec parse_xy_list_ m l =
-  if m == 0
-  then l
-  else parse_xy_list_ (m - 1) ((match (read_line () |> Str.split (Str.regexp " ") |> List.map int_of_string : int list) with | [x;y ] -> x, y | _ -> raise Not_found) :: l)
+let tuple2 a b = (a, b)
 ;;
 
-let parse_xy_list m = parse_xy_list_ m [] |> List.rev
+let curry2 f a b = f (a, b)
+;;
+
+let uncurry2 f (a, b) = f a b
+;;
+
+let (>>) f g x = g (f x)
+;;
+
+let (>>>) f g x y = g (f x y)
+;;
+
+let list_init n f = Array.init n f |> Array.to_list
 ;;
 
 let () =
-  let n, m = match read_line () |> Str.split (Str.regexp " ") |> List.map int_of_string with
-    | [n; m] -> n, m
-    |  _ -> raise Not_found in
-  let p_list = read_line () |> Str.split (Str.regexp " ") |> List.map int_of_string in
-  let xy_list = parse_xy_list m in
-  let uf = uf_make n in
-  List.iter (fun xy -> let x, y = xy in uf_unite uf (x - 1) (y - 1)) xy_list;
-  let res = p_list |> List.mapi (fun i x -> uf_some uf i (x - 1)) |> List.filter (fun x -> x) |> List.length in
+  let n, m = Scanf.scanf "%d %d\n" tuple2 in
+  let p_list = list_init n (fun _ -> Scanf.scanf "%d " dec) in
+  let xy_list = list_init m (fun _ -> Scanf.scanf "%d %d\n" (tuple2 >>> tuple2map dec dec)) in
+  let uf = UF.make n in
+  List.iter (uncurry2 @@ UF.unite uf) xy_list;
+  let res = p_list |> List.mapi tuple2 |> List.filter (uncurry2 @@ UF.some uf) |> List.length in
   Printf.printf "%d\n" res
 ;;
