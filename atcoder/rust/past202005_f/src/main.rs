303fa88881d80e830ae69e9b4bb8ee51c428cb60
kgtkr <kgtkr.jp@gmail.com>
2020-05-23 14:24:13 +0900

    f

---
+++ atcoder/rust/past202005_f/src/main.rs
@@ -0,0 +1,122 @@
+extern crate core;
+
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
+
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
+
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
+
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
+    };
+}
+
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
+        };
+    };
+}
+
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize){n;list:#});
+    let list = list
+        .into_iter()
+        .map(|c| c.chars().collect::<HashSet<_>>())
+        .collect::<Vec<_>>();
+
+    let mut res = Vec::new();
+    for i in 0..(n / 2) {
+        if let Some(x) = list[i].intersection(&list[n - 1 - i]).next() {
+            res.push(x);
+        } else {
+            return "-1".to_string();
+        }
+    }
+
+    if n % 2 == 1 {
+        res.push(list[n / 2].iter().next().unwrap());
+    }
+
+    for i in 0..(n / 2) {
+        res.push(res[n / 2 - i - 1]);
+    }
+    res.into_iter().collect::<String>()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+tests! {
+    test1: "2\nyc\nys\n" => "yy\n",
+    test2: "2\nrv\njh\n" => "-1\n",
+    my_test2: "3\napq\nxyz\nabc\n" => "aza\n",
+}
