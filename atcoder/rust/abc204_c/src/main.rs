a268b9d9f6565ed6e20e401aa4e8e53169e6fafa
kgtkr <kgtkr.jp@gmail.com>
2021-06-10 22:15:31 +0900

    あわあわ

---
+++ atcoder/rust/abc204_c/src/main.rs
@@ -0,0 +1,128 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
+
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
+
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
+
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
+    };
+}
+
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
+        };
+    };
+}
+
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize m:usize){m;list:(@,@)});
+    let mut edges = Vec::new();
+    edges.resize(n, Vec::new());
+    for (a, b) in list {
+        edges[a].push(b);
+    }
+
+    let mut sum = 0;
+
+    for i in 0..n {
+        let mut visited = Vec::new();
+        visited.resize(n, false);
+        let mut stack = Vec::new();
+
+        visited[i] = true;
+        stack.push(i);
+
+        while let Some(a) = stack.pop() {
+            for &b in &edges[a] {
+                if !visited[b] {
+                    stack.push(b);
+                    visited[b] = true;
+                }
+            }
+        }
+
+        sum += visited.iter().filter(|&&x| x).count();
+    }
+    sum.to_string()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "3 3\n1 2\n2 3\n3 2\n" => "7\n",
+    test2: "3 0\n" => "3\n",
+    test3: "4 4\n1 2\n2 3\n3 4\n4 1\n" => "16\n",
+}
