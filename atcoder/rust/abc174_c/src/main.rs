c0a717788b756ee2e652fad41e9fc5fcc8c84c61
kgtkr <contact@kgtkr.net>
2022-07-24 22:21:10 +0900

    pro

---
+++ atcoder/rust/abc174_c/src/main.rs
@@ -0,0 +1,116 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
+
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
+
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
+
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
+    };
+}
+
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
+        };
+    };
+}
+
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(k:i64));
+    if k % 2 == 0 || k % 5 == 0 {
+        return "-1".to_string();
+    }
+    let mut p = 1;
+    let mut r = 0;
+    let mut count = 0;
+    loop {
+        r += 7 * p;
+        r %= k;
+        p *= 10;
+        p %= k;
+        count += 1;
+        if r == 0 {
+            return format!("{}", count);
+        }
+    }
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+    test1: "101\n" => "4\n",
+    test2: "2\n" => "-1\n",
+    test3: "999983\n" => "999982\n",
+}
