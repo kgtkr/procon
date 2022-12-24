ab69280fa0f5f6ffd52086f8faa9a60eb3894871
kgtkr <contact@kgtkr.net>
2022-12-24 16:07:20 +0900

    b

---
+++ atcoder/rust/xmascon22_b/src/main.rs
@@ -0,0 +1,125 @@
+extern crate core;
+
+use std::io::{self, Read};
+
+#[macro_export]
+macro_rules! input {
+  ($s:expr=>$($t:tt)*) => {
+    let mut lines=$s.split("\n");
+    $(
+        line_parse!(lines,$t);
+    )*
+  };
+}
+
+macro_rules! line_parse {
+  ($lines:expr,($($name:ident:$t:tt)*)) => {
+    let mut line=$lines.next().unwrap().split_whitespace();
+    $(value_def!(line,$name,$t);)*
+  };
+
+  //複数行
+  ($lines:expr,{$n:expr;$name:ident:$t:tt}) => {
+    values_def!($lines,$n,$name,$t);
+  };
+}
+
+macro_rules! value_def {
+    ($line:expr, $name:ident, $t:tt) => {
+        let $name = value!($line, $t);
+    };
+}
+
+macro_rules! values_def {
+    ($lines:expr, $n:expr, $name:ident, $t:tt) => {
+        let $name = {
+            let mut vec = Vec::new();
+            for i in 0..$n {
+                let mut next = $lines.next().unwrap().split_whitespace();
+                vec.push(value!(next, $t));
+            }
+            vec
+        };
+    };
+}
+
+macro_rules! value {
+  //配列
+  ($line:expr,[$t:tt]) => {
+    $line.map(|x|{
+      let mut iter=::std::iter::once(x);
+      value!(iter,$t)
+    }).collect::<Vec<_>>()
+  };
+  //タプル
+  ($line:expr,($($t:tt),*)) => {
+    ($(value!($line,$t),)*)
+  };
+  //文字列
+  ($line:expr,#) => {
+    $line.next().unwrap()
+  };
+  //インデックス(-1)
+  ($line:expr,@) => {
+    $line.next().unwrap().parse::<usize>().unwrap()-1
+  };
+  //単一値
+  ($line:expr,$t:ty) => {
+    $line.next().unwrap().parse::<$t>().unwrap()
+  };
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let output = solve(input.trim().to_string());
+    println!("{}", output);
+}
+
+fn solve(input: String) -> String {
+    input!(input=>(n:usize k:usize p:usize)(s:#));
+    let s = s.chars().collect::<Vec<_>>();
+
+    let sums = (0..=(2 * k))
+        .map(|i| match s.get(i) {
+            Some(&'A') => p,
+            Some(&'B') => 100 - p,
+            _ => 50,
+        })
+        .scan(0, |sum, x| {
+            *sum += x;
+            Some(*sum)
+        })
+        .collect::<Vec<_>>();
+    (0..k)
+        .map(|k| {
+            let x = sums[2 * k];
+            let y = (2 * (k + 1) - 1) * 100 - x;
+            if x > y {
+                "+"
+            } else if x < y {
+                "-"
+            } else {
+                "0"
+            }
+        })
+        .collect::<String>()
+}
+
+macro_rules! tests {
+    ($($name:ident: $input:expr=>$output:expr,)*) => {
+        mod tests {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($output.trim().to_string(),super::solve($input.trim().to_string()));
+                }
+            )*
+        }
+    }
+}
+
+tests! {
+  test1: "2 2 25\nAB\n" => "-0\n",
+  test2: "10 4 99\nAABBBAABBB\n" => "++-+\n",
+}
